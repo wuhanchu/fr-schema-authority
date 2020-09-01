184b994180a2f55afd4432949235d05d016bc034
wuhanchu <whcwuhanchu@gmail.com>
2020-09-01 18:50:19 +0800

    新增清理 IE11 的 cookie

--- user/models/login.js
+++ user/models/login.js
@@ -13,6 +13,7 @@ function deleteAllCookies() {
         let eqPos = cookie.indexOf("=");
         let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        document.execCommand("ClearAuthenticationCache");
     }
 }
 
